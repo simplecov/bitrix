ad82bbfb41cfea00a2f4685e0b909cf5038a3504
simplecov <1kbspeed@gmail.com>
2018-07-25 13:39:48 +0300

    Create local folder

---
+++ local/templates/localTemplate/description.php
@@ -0,0 +1,13 @@
+<?
+$arTemplate = array (
+  'NAME' => 'в папке локал',
+  'DESCRIPTION' => 'шаблон',
+  'SORT' => '',
+  'TYPE' => '',
+  'EDITOR_STYLES' => 
+  array (
+    0 => '/bitrix/css/main/bootstrap.css',
+    1 => '/bitrix/css/main/font-awesome.css',
+  ),
+);
+?>
