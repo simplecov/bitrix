7f825d7b7765df58d57c863c21feab74a961de32
simplecov <1kbspeed@gmail.com>
2018-07-27 10:51:27 +0300

    Component class.php

--- local/components/simplecov/currency.rates/class.php
+++ local/components/simplecov/currency.rates/class.php
@@ -1,4 +1,169 @@
-<?php
-class CurrencyRates{
+<?
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+{
+    die();
+}
 
+use Bitrix\Highloadblock\HighloadBlockTable as HLBT;
+
+//\CBitrixComponent::includeComponentClass('highloadblock');
+CModule::IncludeModule('highloadblock');
+
+class CurrencyRates extends CBitrixComponent
+{
+
+    public $errors = array();
+
+    public $arRecievedData = array();
+
+    public $query = '';
+
+    public $baseCurrencyCode = '';
+
+    private $hlBlockID;
+
+
+    /**
+     * Инициализация сайта
+     */
+    public function init()
+    {
+        $this->SetHLBlockId();
+        $this->SetSiteCurrency();
+    }
+
+    /**
+     * Устанавливает ID hl-блока
+     */
+    private function SetHLBlockId()
+    {
+        $this->hlBlockID = HL_CURRENCY;
+    }
+
+    /**
+     * Инициализирует значение валюты сайта
+     */
+    private function SetSiteCurrency()
+    {
+        $this->baseCurrencyCode = CCurrency::GetBaseCurrency();
+    }
+
+    /**
+     * @return string
+     *
+     * Возвращает код валюты сайта
+     */
+    private function GetSiteCurrency()
+    {
+        return $this->baseCurrencyCode;
+    }
+
+
+    /**
+     * @param $HlBlockId
+     * @return bool
+     *
+     * Получаем экземпляр класса для манипуляций с hl-блоком
+     */
+    function GetEntityDataClass($HlBlockId) {
+        if (empty($HlBlockId) || $HlBlockId < 1)
+        {
+            return false;
+        }
+        $hlblock = HLBT::getById($HlBlockId)->fetch();
+        $entity = HLBT::compileEntity($hlblock);
+        $entity_data_class = $entity->getDataClass();
+
+        return $entity_data_class;
+    }
+
+
+    /**
+     * @param $source
+     * @param $date
+     * @return array
+     *
+     *  Выполняет запрос
+     */
+    public function GetQueryData($source, $date)
+    {
+        $query = $this->GetQueryString($source, $date);
+
+        $queryData = file_get_contents($query);
+        $arrData = json_decode($queryData, true);
+
+        return $arrData;
+    }
+
+
+    /**
+     * @param $source
+     * @param $date
+     * @return string
+     *
+     * Сбор строки запроса
+     */
+    private function GetQueryString($source, $date)
+    {
+        $query = $source;
+        $query .= $this->PrepareDate($date);
+        $query .= '?base=' . $this->GetSiteCurrency();
+
+        return $query;
+    }
+
+
+    public function SaveData($arrData)
+    {
+        if(!empty($arrData))
+        {
+            $entity_data_class = $this->GetEntityDataClass($this->hlBlockID);
+            foreach ($arrData['rates'] as $key => $rate)
+            {
+                $result = $entity_data_class::add(array(
+                    'UF_VALUE'        => $rate,
+                    'UF_DATE'         => $this->FormatDateToSite($this->PrepareDate($_GET['date'])),
+                    'UF_CODE'         => $key,
+                ));
+            }
+        }
+    }
+
+    /**
+     * @param $date
+     * @return false|string
+     *
+     * Проверяет дату и возвращает подготовленное значение
+     */
+    public function PrepareDate($date)
+    {
+        return strlen($date) ? $date : date('Y-m-d');
+    }
+
+    /**
+     * @param $date
+     * @return false|string
+     *
+     * Приводим дату к формату сайта
+     */
+    protected function FormatDateToSite($date)
+    {
+        return date('d.m.Y', strtotime($date));
+    }
+
+    /**
+     * @param $date
+     * @return false|string
+     *
+     * Приводим дату к формату компонента
+     */
+    protected function FormatDateToComponent($date)
+    {
+        return date('Y-m-d', strtotime($date));
+    }
+
+    public function sayASD(){
+
+        echo 'ASDASDASDASDDASD';
+    }
 }
--- local/components/simplecov/currency.rates/component.php
+++ local/components/simplecov/currency.rates/component.php
@@ -1,24 +1,6 @@
 <?
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
-use Bitrix\Highloadblock\HighloadBlockTable as HLBT;
-// id highload-инфоблока
-const MY_HL_BLOCK_ID = 1;
-//подключаем модуль highloadblock
-CModule::IncludeModule('highloadblock');
-
-//Напишем функцию получения экземпляра класса:
-function GetEntityDataClass($HlBlockId) {
-    if (empty($HlBlockId) || $HlBlockId < 1)
-    {
-        return false;
-    }
-    $hlblock = HLBT::getById($HlBlockId)->fetch();
-    $entity = HLBT::compileEntity($hlblock);
-    $entity_data_class = $entity->getDataClass();
-    return $entity_data_class;
-}
-
 global $baseCurrency;
 
 /**
@@ -29,64 +11,25 @@ global $baseCurrency;
 if (!$this->InitComponentTemplate())
 	return;
 
-// Код базовой валюты сайта
-$baseCurrency = CCurrency::GetBaseCurrency();
-//wwq($baseCurrency);
+/**
+ * Инициализируем класс
+ */
+$this->init();
 
-switch ($_GET['request'])
-{
-    case 'external':
-        break;
-
-    case 'internal':
-        break;
-}
 /**
  * Обработка внешнего запроса
  */
 if($_GET['request'] == 'external')
 {
-    /**
-     * Дата по запросу пользователя
-     */
-    $date = strlen($_GET['date']) ? $_GET['date'] : date('Y-m-d');
-    wwq('GET');
-    wwq($date);
-    wwq(strtotime($date));
-    wwq(date('d.m.Y', strtotime($date)));
-
-    /**
-     * Сбор строки запроса
-     */
-    $query = $arParams['SOURCE']; // базовый адрес сервиса
-    $query .= $date; // указываем дату
-    $query .= '?base=' . $baseCurrency; // указываем базовую валюту сайта
-    wwq($query);
-
-    //$data = file_get_contents('https://exchangeratesapi.io/api/latest?symbols=USD,RUB,EUR');
-    //$data = file_get_contents('https://exchangeratesapi.io/api/latest?base=RUB');
-    $data = file_get_contents($query);
-    $arrData = json_decode($data, true);
+    $arrData = $this->GetQueryData($arParams['SOURCE'], $_GET['date']);
     wwq($arrData);
 
 
-    /**
-     * Запись данных в hl-блок
-     */
-    $entity_data_class = GetEntityDataClass($arParams['HL_BLOCK_ID']);
-    $formattedDate = date('d.m.Y', strtotime($date));
-    $result = $entity_data_class::add(array(
-      'UF_VALUE'        => '123',
-      'UF_DATE'         => $formattedDate,
-      'UF_CODE'         => 'RUB',
-   ));
-    wwq($result);
-    wwq($formattedDate);
-    wwq(CSite::GetDateFormat("SHORT"));
+//    wwq($result);
+//    wwq($formattedDate);
+//    wwq(CSite::GetDateFormat("SHORT"));
 
 }
-
-
 
 
 $this->IncludeComponentTemplate();
